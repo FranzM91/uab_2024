ae974613c5dfcd21150a6491d32212e4a9745ca1
Franz Mamani <franzmamanich1991@gmail.com>
2024-02-29 18:36:48 -0400

    construyendo nuestro Backend
    Estructurando el proyecto.
    - Business.
    - Data.
    -- Contracts.
    - Mappings.

---
+++ taller de applicaciones de internet/actividades/28022024/BackEnd/Spotify.Server/Spotify.Server/Entities/Album.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Spotify.Server.Entities
+{
+    public class Album
+    {
+        public int IdAlbum { get; set; }
+        public string Titulo { get; set; }
+        public string Portada { get; set; }
+        public int AnioLanzamiento { get; set; }
+        public string Descripcion { get; set; }
+        public List<Artista> Artista { get; set; }
+    }
+}
---
+++ taller de applicaciones de internet/actividades/28022024/BackEnd/Spotify.Server/Spotify.Server/Entities/Artista.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Spotify.Server
+{
+    public class Artista
+    {
+        public int IdArtista { get; set; }
+        public string NombreArtistico { get; set; }
+        public string Nacionalidad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string GeneroMusical { get; set; }
+        public string Genero { get; set; }
+    }
+}
