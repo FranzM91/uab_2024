8442fafbb2d9bc20e31e7b89f72b048a231b313e
Franz Mamani <franzmamanich1991@gmail.com>
2024-03-06 18:39:49 -0400

    problematica de clases resuelta.

--- TAI/actividades/06032024/ReadRSS/ReadRSS/Program.cs
+++ TAI/actividades/06032024/ReadRSS/ReadRSS/Program.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Xml;
+using System.ServiceModel.Syndication;
 
 namespace ReadRSS
 {
     internal class Program
     {
+        static SyndicationFeed readXMLFile()
+        {
+            var urlRSS = "https://devocionmatutina.com/feed/";
+            XmlReader data = XmlReader.Create(urlRSS);
+            var result = SyndicationFeed.Load(data);
+            return result;
+        }
         static void Main(string[] args)
         {
+            var data = readXMLFile();
+            
             Console.WriteLine("****************************************************************************************************");
             Console.WriteLine("Mis matutinas");
+
+            foreach (var item in data.Items)
+            {
+                Console.WriteLine(item.Title.Text);
+            }
             Console.WriteLine("****************************************************************************************************");
             Console.WriteLine();
             Console.ReadKey();
