144aba43fb19587bb11eac67355ec8f9a2054c65
Franz Mamani <franzmamanich1991@gmail.com>
2024-04-17 20:17:33 -0400

    update url

--- TAI/actividades/10042024/main/src/environments/environment.prod.ts
+++ TAI/actividades/10042024/main/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serviceUrl: 'https://fd60-190-129-199-203.ngrok-free.app/api/artista/getall'
+  serviceUrl: 'https://eecc-190-129-199-203.ngrok-free.app/api/artista/getall'
 };
